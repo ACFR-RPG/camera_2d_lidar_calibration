67ad5ecb45fdb267777f13b30c0c26aefc71ada2
YiduoWang47 <yiduowang47@gmail.com>
2026-03-19 00:00:35 +1100

    Add more comments for intrinsic calibration

--- camera_2d_lidar_calibration/cam_intrinsic.py
+++ camera_2d_lidar_calibration/cam_intrinsic.py
@@ -24,12 +24,18 @@ def main():
 
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 
-    objp = np.zeros((11*8, 3), np.float32)
-    objp[:,:2] = np.mgrid[0:8,0:11].T.reshape(-1,2)*0.019
+    # Object Points (objp) refers to the known points on an object, in this case a checkerboard
+    # The known points are the checkerboard vertices (inside, excluding the out most corners)
+    # Depending on your checkerboard layout, the parameters below willc change
+    # TODO: Update the checkerboard parameters based on your own printed out board
+    checkerboard_width = 11
+    checkerboard_height = 8
+    checkerboard_size = 0.19
+    objp = np.zeros((checkerboard_width*checkerboard_height, 3), np.float32)
+    objp[:,:2] = np.mgrid[0:checkerboard_height, 0:checkerboard_width].T.reshape(-1,2)*checkerboard_size
+
     # Arrays to store object points and image points from all the images.
-
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
     
@@ -38,14 +44,15 @@ def main():
     for img in images:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (8,11), None)
+        ret, corners = cv2.findChessboardCorners(gray, (checkerboard_height, checkerboard_width), None)
         # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            # Search in the grayscale image for the corners, refined sub pixel coorindates
+            corners2 = cv2.cornerSubPix(gray, corners, (5, 5), (-1,-1), criteria)
             imgpoints.append(corners2)
             # Draw and display the corners
-            cv2.drawChessboardCorners(img, (8,11), corners2, ret)
+            cv2.drawChessboardCorners(img, (checkerboard_height, checkerboard_width), corners2, ret)
             cv2.imshow('img', img)
             cv2.waitKey(500)
     cv2.destroyAllWindows()
@@ -55,11 +62,11 @@ def main():
     print(mtx)
     print(dist)
 
-# Intrinsics: 
+# Example Intrinsic: 
 # [[519.26845842   0.         331.11197675]
 #  [  0.         518.89359517 229.43433605]
 #  [  0.           0.           1.        ]]
-# Distortion: 
+# Example Distortion: 
 # [[ 0.11418155  0.19343114 -0.00268067  0.00371577 -1.09539701]]
 
 
